dc82ddc48f07f432e976bb08047ab096035ed4a0
Hadi Jafari <haadijafari2003@gmail.com>
2023-12-27 10:45:31 +0330

    File Validation
    [+] Modified the acceptable files

--- book/index/validators.py
+++ book/index/validators.py
@@ -2,6 +2,6 @@ def validate_file_extension(value):
     import os
     from django.core.exceptions import ValidationError
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.pdf', '.doc', '.docx', '.jpg', '.png', '.xlsx', '.xls']
+    valid_extensions = ['.pdf']
     if not ext.lower() in valid_extensions:
-        raise ValidationError('Please select ".pdf" or ".epub" files')
+        raise ValidationError('Please select ".pdf" files')
